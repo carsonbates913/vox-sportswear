8db0915d8ec48204f9053e990d27d95c900aadb0
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-11-04 11:08:06 -0500

    -feat: add new firebase configuration, replace product onSnapshot listeners with async getters, add user specification for cart functions

--- frontend/src/services/datastore.js
+++ frontend/src/services/datastore.js
@@ -3,77 +3,72 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {
-  getDatabase, ref, set, update, remove, onValue, push
-} from 'firebase/database';
+  getFirestore, collection, doc, getDoc, getDocs, addDoc, setDoc, deleteDoc, updateDoc, onSnapshot
+} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAM8MginmCIDm3wF84yEO3OyOTw2tcB49I",
-  authDomain: "vox-sportswear.firebaseapp.com",
-  projectId: "vox-sportswear",
-  storageBucket: "vox-sportswear.appspot.com",
-  messagingSenderId: "111797782567",
-  appId: "1:111797782567:web:a929b40c1e066739bbe602",
-  measurementId: "G-5FMNH39JTE"
+  apiKey: "AIzaSyCKrqm1M6F3Zzp6uDTsqmV0wpCWxNgXa6c",
+  authDomain: "vox-sportswear-b3355.firebaseapp.com",
+  projectId: "vox-sportswear-b3355",
+  storageBucket: "vox-sportswear-b3355.appspot.com",
+  messagingSenderId: "483406817164",
+  appId: "1:483406817164:web:65c9e00079ccae951f58a9",
+  measurementId: "G-45Z989BFQK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getDatabase(app);
+const db = getFirestore(app);
 
 export const initFirebase = () => {
     return app;
 }
 
 /* Returns all clothing data from database */
-export function getAllProducts(callback = () => {}){
-  const reference = ref(db, "Products/");
-  onValue(reference,(snapshot) => {
-    const products = snapshot.val();
-    callback(products); 
-})
+export async function getAllProducts(){
+  const reference = collection(db, "Products");
+  const data = await getDocs(reference);
+  return data;
 }
 
-export function getSpecificProduct(productID, callback = ()=>{}){
-  const reference = ref(db, "Products/" + productID);
-  onValue(reference,(snapshot) => {
-    const product = snapshot.val();
-    callback(product); 
-})
+export async function getSpecificProduct(productID){
+  const reference = doc(db, "Products", productID);
+  const data = await getDoc(reference);
+  return data;
 }
 
-export function getAllCart(callback = ()=>{}){
-  const reference = ref(db, "Cart/");
-  onValue(reference,(snapshot) => {
-    const cartItems = snapshot.val();
-    callback(cartItems); 
-  })
+export async function getAllCart(userID, callback = () => {}){
+  const reference = collection(db, "Users", userID, "Cart");
+  const cancel = onSnapshot(reference, (snapshot) => {
+    callback(snapshot);
+  });
+  return cancel;
 }
 
-export function addToCart(id, product, size){
-  const reference = ref(db,'Cart/' + id);
-  set(reference, {
-      productName: product.productName,
-      price: product.price,
-      size: size,
-      id: id,
-      description: product.description,
-      quantity: 1
-  })
+export async function addToCart(userID, productID, size){
+  const reference = collection(db, "Users", userID, "Cart");
+  const cartItem = { 
+    productID: productID,
+    size: size,
+    quantity: 1
+  }
+  await addDoc(reference, cartItem);
 }
 
-export function deleteFromCart(id) {
-  const reference = ref(db,'Cart/' + id);
-  remove(reference)
+export async function deleteFromCart(id) {
+  const reference = doc(db,'Cart/' + id);
+  await deleteDoc(reference);
 }
 
 export function updateCartQuantity(id, newQuantity) {
-  const reference = ref(db, 'Cart/' + id);
-  update(reference, {
+  const reference = doc(db, 'Cart/' + id);
+  updateDoc(reference, {
     quantity: newQuantity
   })
 }
