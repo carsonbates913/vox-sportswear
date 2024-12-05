01f8c8964d30402ef9e3a9ae3add6ff95d054e1d
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-05 13:07:50 -0500

    -feat: add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage/Homepage.jsx';
-import AboutUs from './pages/AboutUs/AboutUs.jsx';
-import MyCart from './pages/MyCart/MyCart.jsx';
-import Products from './pages/Products/Products.jsx';
-import MyAccount from './auth/MyAccount.jsx';
+import { BrowserRouter as Router} from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes.jsx'
 import NavBar from './components/Navbar/Navbar.jsx';
-import { initFirebase } from './services/datastore.js';
-import { getAuth } from 'firebase/auth';
 import { useAuth } from './context/AuthContext.jsx'
 import './App.css'
 
@@ -21,14 +15,7 @@ function App() {
       <>
         <Router>
           <NavBar />
-          <Routes>
-            <Route path="/aboutus" element={<AboutUs/>}/>
-            <Route path="/products" element={<Products/>}/>
-            <Route path="/mycart" element={<MyCart/>}/>
-            <Route path="/homepage" element={<Homepage/>}/>
-            <Route path="/myaccount" element={<MyAccount/>}/>
-            <Route path="/" element={<Homepage/>}/>
-          </Routes>
+          <AppRoutes />
         </Router>
       </>
     )
