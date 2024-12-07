ea5311f6f3533468c248e55a6702269e0533418d
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-06 23:20:00 -0500

    -feat: add firebase functions

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -7,8 +7,35 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+import functions from "firebase-functions";
+import admin from "firebase-admin";
+
+export const createFirstAdmin = functions.https.onRequest(async (req, res) => {
+  admin.initializeApp();
+  const email = "carson.d.bates.27@dartmouth.edu";
+
+  try {
+    const user = await admin.auth().getUserByEmail(email);
+    await admin.auth().setCustomUserClaims(user.uid, {isAdmin: true});
+    res.status(200).send(`Successfully created first admin role to ${email}`);
+    console.log("worked");
+  } catch (error) {
+    console.error("failed to add first admin:", error);
+    res.status(500).send("Failed to assign admin role. Check logs for details");
+  }
+});
+
+export const sendOrderResponse = functions.https.onRequest(
+    {cors: true},
+    async (req, res) => {
+      try {
+        // const {price, msg} = JSON.parse(req.body);
+        res.status(200).send("Successfuly did somethinggg!");
+      } catch (error) {
+        console.error("Error - ", error);
+        res.status(500).send("Unable to send order response");
+      }
+    });
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
