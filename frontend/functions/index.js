a8c12649e2497e7db7d598aff76eccb12506ee58
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-17 11:59:49 -0500

    -feat: add recipient email functionality and decline order feature

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -48,68 +48,77 @@ export const sendOrderResponse = functions.https.onRequest(
     {cors: true},
     async (req, res) => {
       try {
-        const {price, message, orderID} = req.body;
-        const accessToken = await generateAccessToken();
-        const order = {
-          "intent": "CAPTURE",
-          "purchase_units": [{
-            "amount": {
-              "currency_code": "USD",
-              "value": `100.00`,
-            },
-          }],
-          "payment_source": {
-            "paypal": {
-              "experience_context": {
-                "brand_name": "Vox-Sportswear",
-                "payment_method_preference": "IMMEDIATE_PAYMENT_REQUIRED",
-                "landing_page": "LOGIN",
-                "shipping_preference": "GET_FROM_FILE",
-                "user_action": "PAY_NOW",
-                "locale": "en-US",
-                "return_url": "https://confirmorder-ffshwcchqq-uc.a.run.app", // Add return URL here
-                "cancel_url": "https://example.com/cancel", // Add cancel URL here
+        const {submitType, recipientEmail, price, message, orderID} = req.body;
+        if (submitType==="accept") {
+          const accessToken = await generateAccessToken();
+          const order = {
+            "intent": "CAPTURE",
+            "purchase_units": [{
+              "amount": {
+                "currency_code": "USD",
+                "value": `100.00`,
+              },
+            }],
+            "payment_source": {
+              "paypal": {
+                "experience_context": {
+                  "brand_name": "Vox-Sportswear",
+                  "payment_method_preference": "IMMEDIATE_PAYMENT_REQUIRED",
+                  "landing_page": "LOGIN",
+                  "shipping_preference": "GET_FROM_FILE",
+                  "user_action": "PAY_NOW",
+                  "locale": "en-US",
+                  "return_url": "https://confirmorder-ffshwcchqq-uc.a.run.app", // Add return URL here
+                  "cancel_url": "https://example.com/cancel", // Add cancel URL here
+                },
               },
             },
-          },
-        };
-        const data = JSON.stringify(order);
+          };
+          const data = JSON.stringify(order);
 
-        const response = await fetch("https://api-m.sandbox.paypal.com/v2/checkout/orders", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Paypal-Request-Id": crypto.randomUUID(),
-            "Authorization": `Bearer ${accessToken}`,
-          },
-          body: data,
-        });
+          const response = await fetch("https://api-m.sandbox.paypal.com/v2/checkout/orders", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Paypal-Request-Id": crypto.randomUUID(),
+              "Authorization": `Bearer ${accessToken}`,
+            },
+            body: data,
+          });
 
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          console.log(data.links);
-          const approveLink = data.links.find(
-              (link) => link.rel === "payer-action");
-          if (approveLink) {
-            const msg = {
-              to: "carson.d.bates.27@dartmouth.edu",
-              from: "carson.d.bates.27@dartmouth.edu",
-              subject: "Test Test",
-              text: `price: ${price}, message: 
-              ${message + ` Payment Link: ${approveLink.href}`}`,
-            };
-            await sgMail.send(msg);
-            await db.collection("Orders").doc(orderID).delete();
-            res.status(200).send("Successfuly did something!");
+          if (response.ok) {
+            const data = await response.json();
+            const approveLink = data.links.find(
+                (link) => link.rel === "payer-action");
+            if (approveLink) {
+              const msg = {
+                to: `${recipientEmail}`,
+                from: "carson.d.bates.27@dartmouth.edu",
+                subject: "Test Test",
+                text: `price: ${price}, message: 
+                ${message + ` Payment Link: ${approveLink.href}`}`,
+              };
+              await sgMail.send(msg);
+              await db.collection("Orders").doc(orderID).delete();
+              res.status(200).send("Successfuly did something!");
+            } else {
+              throw new Error("Payment error! could not find approve link");
+            }
           } else {
-            throw new Error("Payment error! could not find approve link");
+            const errorResponse = await response.json();
+            throw new Error(`HTTP error! Status: 
+              ${response.status} - ${response.statusText} - 
+              ${JSON.stringify(errorResponse)}`);
           }
-        } else {
-          const errorResponse = await response.json();
-          throw new Error(`HTTP error! Status: 
-            ${response.status} - ${response.statusText} - 
-            ${JSON.stringify(errorResponse)}`);
+        } else if (submitType==="decline") {
+          const msg = {
+            to: `${recipientEmail}`,
+            from: "carson.d.bates.27@dartmouth.edu",
+            subject: "Test Test",
+            text: `${message}`,
+          };
+          await sgMail.send(msg);
+          await db.collection("Orders").doc(orderID).delete();
         }
       } catch (error) {
         console.error("Error - ", error);
