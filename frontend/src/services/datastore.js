6c78edda20a44382fd3ecadbc5490ba002864143
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-17 12:05:14 -0500

    -feat: update storage bucket link and upload image feature

--- frontend/src/services/datastore.js
+++ frontend/src/services/datastore.js
@@ -5,6 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 import {
   getFirestore, collection, doc, getDoc, getDocs, addDoc, setDoc, deleteDoc, updateDoc, onSnapshot
 } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,7 +15,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCKrqm1M6F3Zzp6uDTsqmV0wpCWxNgXa6c",
   authDomain: "vox-sportswear-b3355.firebaseapp.com",
   projectId: "vox-sportswear-b3355",
-  storageBucket: "vox-sportswear-b3355.appspot.com",
+  storageBucket: "vox-sportswear-b3355.firebasestorage.app",
   messagingSenderId: "483406817164",
   appId: "1:483406817164:web:65c9e00079ccae951f58a9",
   measurementId: "G-45Z989BFQK"
@@ -24,6 +25,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const storage = getStorage(app)
 
 export const initFirebase = () => {
     return app;
@@ -56,19 +58,31 @@ export async function getSpecificCartItem(userID, cartItemID){
   return data;
 }
 
-export async function addToCart(userID, productID, size){
+export async function addToCart(userID, productID, formData){
   const reference = collection(db, "Users", userID, "Cart");
   const cartItem = { 
-    productID: productID,
-    size: size,
-    quantity: 1
+    productID,
+    size: formData.size,
+    color: formData.color,
+    customization: formData.customization,
+    quantity: 1,
+    imageURL: null,
+    imageName: null,
   }
-  await addDoc(reference, cartItem);
+  const docRef = await addDoc(reference, cartItem);
+  if(formData.image){
+    const url = await uploadImage(userID, formData.image);
+    await updateDoc(docRef, {imageURL: url, imageName: formData.image.name});
+  }
 }
 
-export async function deleteFromCart(userID, cartItemID) {
+export async function deleteFromCart(userID, cartItemID, imageName) {
   const reference = doc(db, "Users", userID, "Cart", cartItemID);
   await deleteDoc(reference);
+  if(imageName){
+    console.log(imageName);
+    await deleteImageFromStorage(userID, imageName);
+  }
 }
 
 export async function updateCartQuantity(userID, cartItemID, newQuantity) {
@@ -81,8 +95,8 @@ export async function updateCartQuantity(userID, cartItemID, newQuantity) {
 export async function addOrder(userID, userEmail, items){
   const orderReference = collection(db, "Orders");
   const orderItem = {
-    userEmail: userEmail,
-    items: items,
+    userEmail,
+    items,
   }
   await addDoc(orderReference, orderItem);
 
@@ -101,4 +115,19 @@ export async function getOrders(callback = () => {}){
   const cancel = onSnapshot(reference, (snapshot) => {
     callback(snapshot);
   })
+}
+
+async function uploadImage(userID, image) {
+  if(!image){
+    return;
+  }
+  const imageRef = ref(storage, `Users/${userID}/Cart/${image.name}`);
+  const upload = await uploadBytes(imageRef, image);
+  const url = await getDownloadURL(upload.ref);
+  return url;
+}
+
+async function deleteImageFromStorage(userID, imageName){
+  const imageRef = ref(storage, `Users/${userID}/Cart/${imageName}`);
+  await deleteObject(imageRef);
 }
