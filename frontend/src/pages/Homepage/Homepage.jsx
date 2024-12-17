a64777191545f86ff3bda1b828752ceedc9ce633
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-17 12:02:21 -0500

    -feat: add homepage styling and structure

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -3,24 +3,22 @@ import './Homepage.css'
 const Homepage = () => {
     return (
         <div>
-            <section className="main-content">
-                <div className="logo-container">
-                    <img className="logo-image" src="/assets/Logo.png"></img>
-                </div>
-                <div className="carousel-slider">
-                    <div className=""></div>
-                </div>
-                <div className="space-break"/>
-                <div className="company-title">Vox Sportswear</div>
-                <div className="homeinfo-container">
-                    <div className="homeinfo-selectors">
-                        <div className="homeinfo-select"></div>
-                        <div className="homeinfo-select"></div>
-                        <div className="homeinfo-select"></div>
+            <div className="homepage-main-content">
+                <section className="welcome-section">
+                    <div className="logo-container">
+                        <div className="vox-logo">Vox</div>
+                        <div className="sportswear-logo">sportswear</div>
                     </div>
-                    <div className="homeinfo-text"></div>
-                </div>
-            </section>
+                    <button className="button-shop-now">Shop Now</button>
+                </section>
+                <section className="company-description-section">
+                    <ul className="company-description-container">
+                        <li>Cheaper than Custom</li>
+                        <li>Faster than Online Orders</li>
+                        <li>Student Owned</li>
+                    </ul>
+                </section>
+            </div>
         </div>
     )
 }
