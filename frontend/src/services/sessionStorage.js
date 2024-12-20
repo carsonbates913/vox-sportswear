89310f6480dc2b069958b8711fccf8dcce5c6018
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-20 16:41:44 -0500

    -feat: add a function for changing cart session quantity

--- frontend/src/services/sessionStorage.js
+++ frontend/src/services/sessionStorage.js
@@ -7,6 +7,13 @@ export function retrieveCartFromSession(){
 
 export function addToCartFromSession(cartItem){
   let cart = retrieveCartFromSession();
-  cart.push(cartItem);
+  cart.push({...cartItem, cartItemID: crypto.randomUUID()});
+  sessionStorage.setItem('vox-guestCart', JSON.stringify(cart));
+}
+
+export function updateCartSessionQuantity(cartItemID, newQuantity){
+  let cart = retrieveCartFromSession();
+  let cartItem = cart.find(item => item.cartItemID === cartItemID);
+  cartItem.quantity = newQuantity;
   sessionStorage.setItem('vox-guestCart', JSON.stringify(cart));
 }
