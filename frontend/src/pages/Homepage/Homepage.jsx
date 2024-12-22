26f66264c4e6de61eac5bcca8c10ddd23684d514
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-12-22 13:37:24 -0500

    -feat: polish homepage

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,25 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import './Homepage.css'
 
 const Homepage = () => {
     return (
-        <div>
-            <div className="homepage-main-content">
-                <section className="welcome-section">
-                    <div className="logo-container">
-                        <div className="vox-logo">Vox</div>
-                        <div className="sportswear-logo">sportswear</div>
-                    </div>
-                    <button className="button-shop-now">Shop Now</button>
-                </section>
-                <section className="company-description-section">
-                    <ul className="company-description-container">
-                        <li>Cheaper than Custom</li>
-                        <li>Faster than Online Orders</li>
-                        <li>Student Owned</li>
-                    </ul>
-                </section>
-            </div>
-        </div>
+        <main className="homepage">
+            <section className="homepage__welcome-section">
+                <div className="welcome-section__logo-container">
+                    <div className="logo-container__vox-element">Vox</div>
+                    <div className="logo-container__sportswear-element">sportswear</div>
+                </div>
+                <NavLink className="welcome-section__shop-button" to="/products">Shop Now</NavLink>
+            </section>
+            <section className="homepage__company-description-section">
+                <ul className="company-description-section__description-list">
+                    <li className="description-list__item">Cheaper than Custom</li>
+                    <li className="description-list__item">Faster than Online Orders</li>
+                    <li className="description-list__item">Student Owned</li>
+                </ul>
+            </section>
+        </main>
     )
 }
 
