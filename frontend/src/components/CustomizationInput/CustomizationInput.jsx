8ce1f6cba1ed7297e516f01fe26bb95bfe048e64
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-03-16 17:21:12 -0400

    -feat: change customization input

--- frontend/src/components/CustomizationInput/CustomizationInput.jsx
+++ frontend/src/components/CustomizationInput/CustomizationInput.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react';
 
+import './CustomizationInput.css'
 import ImageUpload from '../ImageUpload/ImageUpload.jsx';
 
-export default function CustomizationInput({register}) {
+export default function CustomizationInput({register, selectedImage}) {
 
-  const [addPersonalDesign, setAddPersonalDesign] = useState(false);
+  const [textValue, setTextValue] = useState('');
 
   return(
     <>
-      <div className="customizeables__personal-design">
-        <p className="customizeables__label" onClick={()=>{setAddPersonalDesign(!addPersonalDesign)}}>Personal Design +</p>
-        {addPersonalDesign && (
+      <div className="customization-input">
+        <p className="customization-input__title">Request Notes</p>
+        {(
             <>
-            <textarea {...register("designNotes")} className="view-product-personal-customization" name="customization"/>
-            <ImageUpload className="custom-image" id="customImage"/>
+            <textarea {...register("designNotes")} className="view-product-personal-customization"/>
+            <ImageUpload register={register} className="custom-image" id="customImage" selectedImage={selectedImage}/>
             <label className="image-customization-label">
             </label>
             </>
