ba78fed5a263a5bb87756fd58e00a67127ff1eb7
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-04-04 02:48:14 -0400

    -feat: fix loading module

--- frontend/src/components/LoadingModule/LoadingModule.jsx
+++ frontend/src/components/LoadingModule/LoadingModule.jsx
@@ -2,19 +2,9 @@ import './LoadingModule.css';
 
 export default function LoadingModule(props) {
 
-  if(props.modal) {
-    return (
-      <></>
-      <div className="loader-modal">
-      <div className="loader"></div>
-    </div>
-    )
-
-  }else{
     return (
       <div className="loader-container" style={props.viewport && { height: "calc(100vh - 80px)" }}>
         <div className="loader"></div>
       </div>
     )
-  }
 }
