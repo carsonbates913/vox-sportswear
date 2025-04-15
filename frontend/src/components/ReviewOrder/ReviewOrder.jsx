7f117e9f796cb12d3b4173623b925d7075bba0cb
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-04-15 00:19:58 -0400

    -feat: fix reviewOrder

--- frontend/src/components/ReviewOrder/ReviewOrder.jsx
+++ frontend/src/components/ReviewOrder/ReviewOrder.jsx
@@ -15,7 +15,7 @@ export default function ReviewOrder({selectedOrder}) {
     <div className="review-order">
       <div className="review-order__item-list">
           {selectedOrder.items.map((item, index)=> {
-              return (<button className="review-order__item"key={index} onClick={()=> {setSelectedItem(index)}}>{item.product}</button>);
+              return (<button className={`review-order__item ${selectedItem === index ? "active" : ""}`} key={index} onClick={()=> {setSelectedItem(index)}}>{item.product}</button>);
           })}
       </div>
       <CartItem item={selectedOrder.items[selectedItem ] || selectedOrder.items[0]} noRemove />
